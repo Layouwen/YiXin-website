108564ba0d6f3ddd3cb177bca3355235b65aee98
Layouwen <Layouwen@gmail.com>
2020-07-08 19:37:51 +0800

    deploy

---
+++ precache-manifest.c52f76c9b3863ea349eac818c28c1227.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e9320ddb0e86e92277",
+    "url": "/YiXin-website/css/app.617f59d6.css"
+  },
+  {
+    "revision": "971480f546c9ac270c87835666e9b06c",
+    "url": "/YiXin-website/index.html"
+  },
+  {
+    "revision": "b8e9320ddb0e86e92277",
+    "url": "/YiXin-website/js/app.fd3e5545.js"
+  },
+  {
+    "revision": "709ab425c7f7b80f8494",
+    "url": "/YiXin-website/js/chunk-vendors.f47839ef.js"
+  },
+  {
+    "revision": "5b549a3e02317dde8efe4dd7ec2a14d8",
+    "url": "/YiXin-website/manifest.json"
+  },
+  {
+    "revision": "c0e52792bb82ad26363fbbe926393858",
+    "url": "/YiXin-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/YiXin-website/robots.txt"
+  }
+]);
