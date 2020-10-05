a41ec543ca7213dfe81dc9e70ea703ebd20bcf10
Layouwen <Layouwen@gmail.com>
2020-10-06 01:02:05 +0800

    deploy

---
+++ precache-manifest.6fc1b7f5ce04d7ebab3b5498b2f4cdfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83cfc9820699cd953e2",
+    "url": "/yixin-money-vue-website/css/app.555aba4e.css"
+  },
+  {
+    "revision": "46ef12cf676ff61ee05956379eae2736",
+    "url": "/yixin-money-vue-website/index.html"
+  },
+  {
+    "revision": "d83cfc9820699cd953e2",
+    "url": "/yixin-money-vue-website/js/app.c648beab.js"
+  },
+  {
+    "revision": "709ab425c7f7b80f8494",
+    "url": "/yixin-money-vue-website/js/chunk-vendors.f47839ef.js"
+  },
+  {
+    "revision": "5b549a3e02317dde8efe4dd7ec2a14d8",
+    "url": "/yixin-money-vue-website/manifest.json"
+  },
+  {
+    "revision": "c0e52792bb82ad26363fbbe926393858",
+    "url": "/yixin-money-vue-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/yixin-money-vue-website/robots.txt"
+  }
+]);
